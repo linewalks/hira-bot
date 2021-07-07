45226fd919e58de9185a3b42714d615e729d82eb
sy.yun <sy.yun@linewalks.com>
2021-07-07 18:53:25 +0900

    chore : add sh script and debug messages.

---
+++ bin/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+> nohup.out
+nohup python3 -u nhiss_bot.py&
+tail -f nohup.out
+
+
--- nhiss_bot.py
+++ nhiss_bot.py
@@ -1,4 +1,5 @@
 import time
+from typing import List
 import requests
 from datetime import timedelta, datetime
 from selenium import webdriver
@@ -8,52 +9,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-# # 지점 선택
-# # TODO: 순서에 따라 지점을 선택해가도록 변경
-# success = False
-# for each_branch in branch_list:
-#   wait.until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="cnfcPrSeatUseAplVO"]/fieldset/table[1]/thead/tr/th[{each_branch[0]}]/button')))
-  
-#   driver.find_element_by_xpath(f'//*[@id="cnfcPrSeatUseAplVO"]/fieldset/table[1]/thead/tr/th[{each_branch[0]}]/button').click()
-#   driver.implicitly_wait(2)
-  
-#   wait.until(EC.text_to_be_present_in_element((By.XPATH, '//*[@id="bdc_title"]'), each_branch[2]))
-#   #wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/section/section[3]/div[1]/form/fieldset/table[2]")))
-
-#   items = driver.find_elements_by_xpath("//table/tbody/tr/td/span/span[not(@class='bdc_ico_ing') and not(@class='bdc_ico_done')]")
-#   if not items:
-#     continue
-  
-#   btn_list = []
-#   temp_key = None
-
-#   for each in items:
-#     each_key = each.get_attribute("id").split("_")[1]
-#     if temp_key != each_key:
-#       temp_key = each_key
-#       btn_list = [each]
-#     else:
-#       btn_list.append(each)
-#       driver.find_element_by_id(each_key).click()
-#       wait.until(EC.element_to_be_clickable((By.ID, each.get_attribute("id"))))
-#       for btn in btn_list: btn.click()
-#       driver.find_element_by_id("btnNext").click()
-#       success = True
-#       break
-  
-#   if success:
-#     break
-
-
 class NhissBot:
   
   def __init__(self, 
       os: str, 
       research_center_xpath: str,
-      research_number_xpath: str):
+      research_number_xpath: str,
+      debug=False):
+    
+    self.debug = debug
     self.os = os
-    # self.driver = webdriver.Chrome(f'./files/driver/{self.os}/chromedriver')
-    self.driver = None
+    if self.debug:
+      self.driver = webdriver.Chrome(f'./files/driver/{self.os}/chromedriver')
+    else:
+      self.driver = None
     # 센터구분 xpath
     self.research_center_xpath = research_center_xpath
     # 연구과제관리번호 선택
@@ -76,7 +45,7 @@ class NhissBot:
       hour=hour, 
       minute=minute, 
       second=second)
-    print(f'[HiraBot] Target Time (KST) {target_datetime}')
+    print(f'[HiraBot] Target  Time (KST): {target_datetime}')
     time_diff = timedelta(hours=9) 
     format = "%a, %d %b %Y %H:%M:%S %Z"
     cur_gmt_time = requests.get('https://opendata.hira.or.kr/home.do').headers['Date']
@@ -87,7 +56,11 @@ class NhissBot:
     delta = target_datetime - cur_kst_time
     time_to_wait_sec = float(delta.total_seconds())
     print(f'[HiraBot] Waiting for: {time_to_wait_sec}s')
-    time.sleep(time_to_wait_sec)
+    try:
+      time.sleep(time_to_wait_sec)
+    except ValueError:
+      print('[HiraBot][TargetTimeError] Target Time must be in the future')
+      exit(1)
     print('[HiraBot] Time to activate HiraBot!')
     self.driver = webdriver.Chrome(f'./files/driver/{self.os}/chromedriver')
 
@@ -96,7 +69,12 @@ class NhissBot:
     self.user_pwd = pwd
     self.user_name = name
   
+  def setResearchers(self, researchers: List[str]):
+    self.researchers = researchers
+
   def login(self):
+    print(f'[HiraBot] Log-in as \
+    \n    Id:{self.user_id}\n    Name:{self.user_name}')
     # 로그인 페이지 접속
     self.wait = WebDriverWait(self.driver, timeout=1)
     self.driver.get('https://nhiss.nhis.or.kr/bd/ay/bdaya003iv.do')
@@ -115,18 +93,23 @@ class NhissBot:
   
 
   def selectReservationOptions(self):
+    print('[HiraBot] Go to My service view.')
     self.__goToMyService()
     time.sleep(1)
-
+    print('[HiraBot] Selecting research number')
     self.__select_research_number()
+    print('[HiraBot] Selecting research center')
     self.__select_research_center()
+    print('[HiraBot] Selecting reservation date')
     self.__select_reservation_date()
+    print('[HiraBot] Selecting visiter(s)')
     self.__select_visitor()
   
 
   def apply(self):
     time.sleep(1)
     # Switch to default frame from cmsView
+    print('[HiraBot] Submitting...')
     self.driver.switch_to.frame("cmsView")
     self.driver.find_element_by_xpath('//*[@id="ods_WSF_1_insert_BTN_APPLY"]').click()
 
@@ -139,12 +122,13 @@ class NhissBot:
     self.driver.quit()
 
 
+  # 연구과제관리번호 선택
   def __select_research_number(self):
-    # 연구과제관리번호 선택
-    # tr[순서 + 1]
+    
     self.driver.find_element_by_id("WSF_1_insert_APLY_MGMT_NO_label").click()
     self.driver.find_element_by_xpath(self.research_number_xpath).click()
-    
+    self.driver.find_element_by_xpath('//*[@id="a"]/table/tbody')
+
 
   # MY서비스 - 분석센터이용 페이지로 이동
   def __goToMyService(self):
@@ -152,20 +136,15 @@ class NhissBot:
     time.sleep(1)
     self.driver.switch_to.frame("cmsView")
 
+
   # 센터구분
   def __select_research_center(self):
     self.driver.find_element_by_id("WSF_1_insert_ZN_CEN_CD_label").click()
-
-    # driver.find_element_by_xpath('/html/body/div[6]/div/table/tbody/tr[2]/td[2]').click() # 서울
     self.driver.find_element_by_xpath(self.research_center_xpath).click() # 광주
-    # element = driver.find_element_by_xpath('//*[@id="a"]/table/tbody/tr[2]/td[2]') # 서울
-    # driver.execute_script(("arguments[0].click();", element))
-    # driver.find_element_by_xpath('//*[@id="a"]/table/tbody/tr[4]/td[2]').click() # 수원
-    
-  
+
+
+  # 예약일자 선택
   def __select_reservation_date(self):
-    # 예약일자 선택
-    # time.sleep(1)
     self.driver.find_element_by_id("ods_WSF_1_insert_BTN_DT").click()
     time.sleep(1)
     # Switch to default frame from cmsView
@@ -173,9 +152,6 @@ class NhissBot:
 
     # Get target day which is two weeks later than today.
     target_day = (datetime.now() + timedelta(weeks=2)).strftime("%Y-%m-%d")
-    print(target_day)
-
-
     self.driver.execute_script("""
       var target_day =  arguments[0]
       var row_count = window[2].WShtAC_1.GetRowCount()
@@ -201,41 +177,63 @@ class NhissBot:
     self.driver.find_element_by_id("ods_WSF_1_insert_BTN_VISTM").click()
     time.sleep(1)
     self.driver.switch_to.default_content()
-    self.driver.execute_script("""
 
-      var target_user_name =  arguments[0]
-      var row_count = window[2].WShtAC_1.GetRowCount()
-      var target_index = -1
-      for (var i = 0; i < row_count; i++){
-        var cur_user_id = window[2].WShtAC_1.GetGridCellText("RSCHR_NM", i)  
-        if (target_user_name == cur_user_id){
-          target_index = i
-          break;
-        }   
-      }
+    def select_visitor_js(researcher_name):
+      self.driver.execute_script("""
+        var target_user_name =  arguments[0]
+        var row_count = window[2].WShtAC_1.GetRowCount()
+        var target_index = -1
+        for (var i = 0; i < row_count; i++){
+          var cur_user_id = window[2].WShtAC_1.GetGridCellText("RSCHR_NM", i)  
+          if (target_user_name == cur_user_id){
+            target_index = i
+            break;
+          }   
+        }
 
-      if (target_index != -1){
-        window[2].WShtAC_1.SetGridCellText("CHK", target_index, 1)
-      }
-      window[2].BTN_SELECT_Click()
-    """, self.user_name)
+        if (target_index != -1){
+          window[2].WShtAC_1.SetGridCellText("CHK", target_index, 1)
+        }
+      """, researcher_name)
+    print("[HiraBot] Researchers:")
+    for researcher_name in self.researchers:
+      select_visitor_js(researcher_name)
+      print(f"    {researcher_name}")
+    self.driver.execute_script("window[2].BTN_SELECT_Click()")
 
 
 if __name__ == "__main__":
+  import sys
+  sys.stdout.flush()
   nhiss_bot = NhissBot(
   os='linux',
+  #TODO: Put research center xpath below.
   research_center_xpath= '//*[@id="a"]/table/tbody/tr[6]/td[2]',
-  research_number_xpath= '//*[@id="a"]/table/tbody/tr[3]/td[2]'
+  #TODO: Put research number xpath below.
+  research_number_xpath= '//*[@id="a"]/table/tbody/tr[3]/td[2]',
+  debug=False
   )
-  nhiss_bot.wait_until_kst(2021, 7, 7, 16, 45, 55)
+
+  #TODO: Set the date time to be the target time below.
+  nhiss_bot.wait_until_kst(2021, 7, 7, 18, 52, 35)
+
+  #TODO: Enter your credentail below.
   nhiss_bot.setCredential(
     id= 'kokoko1230',
     pwd= 'password2@',
     name='박근우'
   )
 
-  nhiss_bot.login()
+  #TODO: Enter researcher name(s) below.
+  researchers = [
+    '성기훈',
+    '문여래',
+    '박근우'
+  ]
 
+  nhiss_bot.setResearchers(researchers)
+  nhiss_bot.login()
   nhiss_bot.selectReservationOptions()
+  #TODO: Comment out when executed for real reservation.
   # nhiss_bot.apply()
-  # nhiss_bot.quit()
+  nhiss_bot.quit()
