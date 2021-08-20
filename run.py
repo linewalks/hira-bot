651ccf732a6dc1159b522df33919f72f8f36ce25
yunsangr <sy.yun@linewalks.com>
2021-08-20 19:10:25 +0900

    chore: add arg parser

--- run.py
+++ run.py
@@ -101,7 +101,29 @@ def handle_exception():
 
 
 if __name__ == "__main__":
-  send_message("공단 예약 신청을 시작합니다.")
-  #TODO: 파이썬 실행 커맨드라인 옵션 추가
-  # run_on_time()
-  handle_exception()
+  from argparse import ArgumentParser, RawTextHelpFormatter
+  parser = ArgumentParser(
+      description=
+"""
+공단 신청을 자동으로 실행하는 봇 프로그램 입니다.
+  신청 실행 모드:
+    1. run_on_time: 세팅한 날짜/시간에 2주 뒤 날짜를 예약 시도하는 모드. 단일 실행
+    2. run_until_success: 세팅한 날짜/시간에 예약이 성공할 때까지 계속해서 시도하는 모드.
+
+""",
+      formatter_class=RawTextHelpFormatter)
+
+  parser.add_argument(
+      "--run_until_success",
+      type=bool,
+      required=False,
+      default=False,
+      help='예약이 성공할 때까지 계속해서 신청 하는 옵션 (사용하지 않으면 run_on_time 모드로 동작)'
+  )
+
+  args = parser.parse_args()
+  print(args.run_until_success)
+  # send_message("공단 예약 신청을 시작합니다.")
+  # #TODO: 파이썬 실행 커맨드라인 옵션 추가
+  # # run_on_time()
+  # handle_exception()
