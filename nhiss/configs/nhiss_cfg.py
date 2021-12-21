36b1ebb7e8823109c8724b5be781517d59b466aa
geunu <geunu@linewalks.com>
2021-12-21 15:29:02 +0900

    Update nhiss_cfg.py

--- nhiss/configs/nhiss_cfg.py
+++ nhiss/configs/nhiss_cfg.py
@@ -1,9 +1,9 @@
 OS = 'windows' # Nhiss Bot이 실행되는 OS
-# /html/body/div[5]/div/table/tbody/tr[2]/td[2]
 RESEARCH_NUMBER_XPATH = '/html/body/div[5]/div/table/tbody/tr[2]/td[2]' #TODO: 연구과제관리번호 xpath 등록. 기본값: 첫번째 연구과제
+
 # RESEARCH_CENTER_XPATH = '/html/body/div[6]/div/table/tbody/tr[2]/td[2]' #빅데이터 분석센터 (서울) 서울은 full xpath 사용
-# RESEARCH_CENTER_XPATH = '/html/body/div[6]/div/table/tbody/tr[4]/td[2]' #경인지역본부 분석센터 xpath
-RESEARCH_CENTER_XPATH = '/html/body/div[6]/div/table/tbody/tr[6]/td[2]' #광주지역본부 분석센터 xpath
+RESEARCH_CENTER_XPATH = '/html/body/div[6]/div/table/tbody/tr[4]/td[2]' #경인지역본부 분석센터 xpath
+# RESEARCH_CENTER_XPATH = '/html/body/div[6]/div/table/tbody/tr[6]/td[2]' #광주지역본부 분석센터 xpath
 CREDENTIAL_ID = ''                                   #TODO: NHISS 접속 유저 정보 등록.
 CREDENTIAL_PWD = ''
 CREDENTIAL_NAME = '박근우'
