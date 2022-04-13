9342d183d30b5c07dbc563d3372475375abef25b
edit8080 <leetaehee0205@gmail.com>
2022-04-13 15:48:01 +0900

    refactoring: split elapsed time check function

--- utils/helper.py
+++ utils/helper.py
@@ -52,3 +52,10 @@ def validate(date_text):
       datetime.datetime.strptime(date_text, '%Y-%m-%d')
   except ValueError:
       raise ValueError(f"Incorrect data format, should be YYYY-MM-DD. Given date_test: {date_text}")
+
+# 시작 시간부터 경과 시간 체크
+def check_elapsed_time(start_time):
+  current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+  end_time = time.time()
+  elapsed = end_time - start_time
+  print(f"[Bot][DEBUG] Current Time: {current_time} Time elapsed (in seconds): {float(elapsed):.2f}")
