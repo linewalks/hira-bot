359587722a52308f0a6610caf4c1511aff04ad40
edit8080 <leetaehee0205@gmail.com>
2022-04-15 14:49:24 +0900

    refactoring(nhiss): split target day convert message about am, pm

--- nhiss/tasks/reservation_mode.py
+++ nhiss/tasks/reservation_mode.py
@@ -2,11 +2,15 @@ import time
 from datetime import timedelta, datetime
 
 from utils.helper import count_down, send_message, check_elapsed_time
-from utils.message import success_msg, failure_msg
+from utils.message import convert_target_day, success_msg, failure_msg
 from nhiss.tasks.common import init_nhiss_bot, reservation_content_fill, click_reservation_button
 
 def run_on_time(info, headless: bool = False, debug: bool = True):
   register_info = info.getInfo()
+  target_day_msg = register_info['target_day']
+
+  if register_info['is_seoul']:
+    target_day_msg = convert_target_day(register_info['target_day'], register_info['is_register_am'])
 
   start_time = time.time()
   today = datetime.now()
@@ -20,25 +24,25 @@ def run_on_time(info, headless: bool = False, debug: bool = True):
       bot.wait_until_kst(today.year, today.month, today.day, 23, 55, 0)
 
     # 1. 예약 신청 내용 입력 (날짜 선택 x)
-    result = reservation_content_fill(bot, register_info['target_day'], register_info['is_seoul'], check_date = False)
+    result = reservation_content_fill(bot, register_info['target_day'], register_info['is_register_am'], register_info['is_seoul'], check_date = False)
 
     if not debug:
       # TODO: NHISS Bot을 실행시킬 시간(예약 실행 시간)을 설정.
       bot.wait_until_kst(next_day.year, next_day.month, next_day.day, 0, 0, 0)
 
     # 2. 갱신된 날짜 테이블 내에서 선택
-    bot.selectReservationDate(is_register_am = register_info['is_register_am'], is_seoul = register_info['is_seoul'])
+    bot.selectReservationDate(register_info['target_day'], register_info['is_register_am'], register_info['is_seoul'])
     
     # 3. 예약 신청
     is_reservation_success = click_reservation_button(bot, debug) 
     # TODO: 연구 과제 중복 신청 예외처리 필요 (현재 중복되어도 성공 출력)
     
     if is_reservation_success:
-      send_message(success_msg(register_info['user_name'], register_info['region'], register_info['target_day'], result['reservation_research_name']))
+      send_message(success_msg(register_info['user_name'], register_info['region'], target_day_msg, result['reservation_research_name']))
 
   except Exception as err:
     print(err)
-    send_message(failure_msg(register_info['user_name'], register_info['region'], register_info['target_day']))
+    send_message(failure_msg(register_info['user_name'], register_info['region'], target_day_msg))
   finally:
     check_elapsed_time(start_time)
     time.sleep(10)
@@ -60,14 +64,14 @@ def run_until_success(info, headless: bool = False):
         # 2. 예약 신청
         is_reservation_success = click_reservation_button(bot) 
 
-        if is_reservation_success: 
-          target_day = register_info['target_day']
+        if is_reservation_success:
+          target_day_msg = register_info['target_day']
 
           if register_info['is_seoul']:
-            target_day += '(오전)' if register_info['is_register_am'] else '(오후)'
+            target_day_msg = convert_target_day(register_info['target_day'], register_info['is_register_am'])
 
           # TODO: 연구 과제 중복 신청 예외처리 필요 (현재 중복되어도 성공 출력)
-          send_message(success_msg(register_info['user_name'], register_info['region'], target_day, result['reservation_research_name']))
+          send_message(success_msg(register_info['user_name'], register_info['region'], target_day_msg, result['reservation_research_name']))
           break
 
     except Exception as err:
--- utils/message.py
+++ utils/message.py
@@ -1,3 +1,6 @@
+def convert_target_day(target_day, is_register_am):
+  return target_day + '(오전)' if is_register_am else '(오후)'
+
 def success_msg(user_name, region, target_day, research_name):
   return f"[Bot] {user_name}님 {region}지역 공단봇 예약 성공하였습니다! target day: {target_day}\n• 연구명: {research_name}"
 
