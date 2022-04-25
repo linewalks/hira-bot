fae0ad5779c68ac0cf0b821ddb6b1b3eda9ece88
jindex2411 <js.jeong@linewalks.com>
2022-04-25 19:51:38 +0900

    feat(Schema): add Schema description

--- main/schema/nhiss.py
+++ main/schema/nhiss.py
@@ -5,23 +5,43 @@ from main.common.common import region_list
 
 # schema
 class ReservationNhissOnTime(Schema):
-  name = fields.Str(required=True)
-  id = fields.Str(required=True)
-  password = fields.Str(required=True)
-  is_seoul = fields.Bool(required=True)
-  research_number = fields.Int(required=True)
+  name = fields.Str(
+      required=True,
+      description="예약 하는 사용자의 이름"
+  )
+  id = fields.Str(
+      required=True,
+      description="NHISS 로그인 아이디"
+  )
+  password = fields.Str(
+      required=True,
+      description="NHISS 로그인 패스워드"
+  )
+  is_seoul = fields.Bool(
+      required=True,
+      description="PC예약신청(서울) 유무"
+  )
+  research_number = fields.Int(
+      required=True,
+      description="연구과제관리번호 ex) 1 첫번째 위치의 연구과제"
+  )
   region = fields.Str(
       validate=validate.OneOf(region_list),
-      required=True
+      required=True,
+      description="센터 지역"
   )
   research_visiter_list = fields.List(
       fields.Str(),
-      required=True
+      required=True,
+      description="방문자 등록"
   )
 
 
 class ReservationNhissUntilSuccess(ReservationNhissOnTime):
-  target_day = fields.Str(required=True)
+  target_day = fields.Str(
+      required=True,
+      description="target_day: XXXX-XX-XX 형식으로 기입"
+  )
 
 
 class ReservationNhissCancel(Schema):
